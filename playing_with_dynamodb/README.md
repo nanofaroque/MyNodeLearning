16ded7eeb5feab51573c59a3f51a417a9a6ce54d
Omar Faroque (Anik) <faroque.ae@gmail.com>
2020-02-16 19:06:21 -0600

    first insertion into dynamodb

---
+++ playing_with_dynamodb/README.md
@@ -0,0 +1,10 @@
+### Running DynamoDb locally
+1. Install docker
+2. Run the command below
+```$xslt
+docker run -p 8000:8000 amazon/dynamodb-local -jar DynamoDBLocal.jar -inMemory -sharedDb
+```
+### NoSQL workbench installation
+
+1. Go to the url below and start
+https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.html
--- playing_with_dynamodb/routes/users.js
+++ playing_with_dynamodb/routes/users.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require("aws-sdk");
+AWS.config.update({
+    region: "local",
+  endpoint: "http://localhost:8000"
+});
 
+var docClient = new AWS.DynamoDB.DocumentClient();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+
+    var table = "PendingApprovalRequest";
+
+    var userId = 'nanofaroque';
+    var dataSourceId = "nanofaroque#The Big New Movie";
+
+    var params = {
+        TableName:table,
+        Item:{
+            "userId": userId,
+            "dataSourceId": dataSourceId,
+            "info":{
+                "plot": "Nothing happens at all.",
+                "rating": 0
+            }
+        }
+    };
+
+    console.log("Adding a new item...");
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
   res.send('respond with a resource');
 });
 
